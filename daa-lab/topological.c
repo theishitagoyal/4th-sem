88c79000e433eb9572d4cf6d4d1a417446b5d251
theishitagoyal <155839993+theishitagoyal@users.noreply.github.com>
2025-05-12 02:55:18 +0530

    Update topological.c

--- daa-lab/topological.c
+++ daa-lab/topological.c
@@ -8,7 +8,7 @@ int adj[MAX][MAX], indegree[MAX], topOrder[MAX];
 void createAdj(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i != j) {
+            if (i < j) {
                 adj[i][j] = rand() % 2;
             } else {
                 adj[i][j] = 0;
